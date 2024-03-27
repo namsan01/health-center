5bdc8a4f685738f91bed5a20f18cee81ead4f380
namsan01 <namsan2934@gmail.com>
2024-03-27 10:44:41 +0900

    sitemap , robots.txt 적용

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://health-center-namsan01.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
